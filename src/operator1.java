2dbaa3e3d1f50f7268dd068129505eae0aecb447
herreang000 <herreang000@warren.k12.in.us>
2024-08-08 11:57:13 -0400

    Final

--- src/operator1.java
+++ src/operator1.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class operator1 {
+public class operator1 {
+    public static void main(String[] args) {
+        double myFirstValue = 20.00;
+        double mySecondValue = 80.00;
+        double myValuesTotal = (myFirstValue + mySecondValue) * 100.00;
+        double theRemainder = myValuesTotal % 40.00;
+        boolean isNoRemainder = (theRemainder == 0) ? true : false;
+        System.out.println("isNoRemainder = " + isNoRemainder);
+        if(!isNoRemainder) {
+            System.out.println("Got some remainder");
+        }
+
+    }
 }
